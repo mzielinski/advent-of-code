2ba7a652e132e5e97e24268e4fbb3d2eeb8101c7
maciezie <maciej.zielinski@idemia.com>
2020-12-13 19:16:18 +0100

    Day13 → part 2, not efficient ;(

--- src/main/java/com/mzielinski/advent/of/code/day13/Day13.java
+++ src/main/java/com/mzielinski/advent/of/code/day13/Day13.java
@@ -3,39 +3,45 @@ package com.mzielinski.advent.of.code.day13;
 import com.mzielinski.advent.of.code.utils.ReadFile;
 
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
-import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.stream.Collectors.toSet;
 
 public class Day13 {
 
-    record Note(long timestamp, Set<Integer> buses) {
+    record Note(BigDecimal timestamp, List<BusId> buses) {
     }
 
-    record Result(long waitTime, Integer busId) {
+    record BusId(int index, String busId) {
+
+        public boolean isValidId() {
+            return busId.matches("[\\d]+");
+        }
+
+        public Integer getBusId() {
+            return Integer.parseInt(busId);
+        }
     }
 
     static class NoteReader {
 
         static Note readFile(String filePath) {
-            long timestamp = 0;
-            Set<Integer> busIds = new HashSet<>();
+            BigDecimal timestamp = BigDecimal.ZERO;
+            List<BusId> busIds = new ArrayList<>();
             InputStream stream = requireNonNull(ReadFile.class.getClassLoader().getResourceAsStream(filePath));
             try (Scanner scanner = new Scanner(stream)) {
                 while (scanner.hasNextLine()) {
                     if (scanner.hasNextLong()) {
-                        timestamp = scanner.nextLong();
+                        timestamp = BigDecimal.valueOf(scanner.nextLong());
                     } else if (scanner.hasNext()) {
-                        busIds = Arrays.stream(scanner
-                                .next()
-                                .split(","))
-                                .filter(a -> a.matches("[\\d]+"))
-                                .map(Integer::parseInt)
-                                .collect(toSet());
+                        String[] split = scanner.next().split(",");
+                        for (int index = 0; index < split.length; index++) {
+                            busIds.add(new BusId(index, split[index]));
+                        }
                     }
                 }
             }
@@ -43,20 +49,39 @@ public class Day13 {
         }
     }
 
-    public long findTheEarliestBus(String filePath) {
+    public long findTheEarliestBusPart1(String filePath) {
         Note note = NoteReader.readFile(filePath);
-        Result result = extracted(note);
-        return result.waitTime * result.busId;
+        BigDecimal nextTimestamp = note.timestamp;
+        while (true) {
+            for (BusId busId : note.buses) {
+                if (!busId.isValidId()) continue;
+                if (nextTimestamp.longValue() % busId.getBusId() == 0) {
+                    final BigDecimal foundTimeStamp = nextTimestamp.subtract(note.timestamp);
+                    return foundTimeStamp.multiply(BigDecimal.valueOf(busId.getBusId())).longValue();
+                }
+            }
+            nextTimestamp = nextTimestamp.add(BigDecimal.ONE);
+        }
     }
 
-    private Result extracted(Note note) {
-        long index = note.timestamp;
+    public long findTheEarliestBusPart2(String filePath) {
+        Note note = NoteReader.readFile(filePath);
+        BigDecimal nextTimestamp = note.timestamp;
         while (true) {
-            index++;
-            for (Integer busId : note.buses) {
-                if (index % busId == 0) {
-                    return new Result(index - note.timestamp, busId);
-                }
+            BigDecimal finalNextTimestamp = BigDecimal.valueOf(nextTimestamp.longValue());
+            System.out.println(finalNextTimestamp);
+            final Optional<BusId> bus = note.buses.stream()
+                    .filter(BusId::isValidId)
+                    .filter(busId -> {
+                        final BigDecimal nextTimeStamp = finalNextTimestamp.add(BigDecimal.valueOf(busId.index));
+                        return nextTimeStamp.longValue() % busId.getBusId() != 0;
+                    })
+                    .findAny();
+            if (bus.isEmpty()) {
+                // found valid sequence
+                return finalNextTimestamp.longValue();
+            } else {
+                nextTimestamp = finalNextTimestamp.add(BigDecimal.ONE);
             }
         }
     }
--- src/test/java/com/mzielinski/advent/of/code/day13/Day13Test.groovy
+++ src/test/java/com/mzielinski/advent/of/code/day13/Day13Test.groovy
@@ -6,16 +6,29 @@ import spock.lang.Unroll
 class Day13Test extends Specification {
 
     @Unroll
-    def 'should calculate #result the earliest bus multiplied by wait time for file #filepath'() {
-        when:
-        def day13 = new Day13()
-
-        then:
-        day13.findTheEarliestBus(filepath) == result
+    def 'should calculate #result the earliest bus multiplied by wait time for file #filepath in part 1'() {
+        expect:
+        new Day13().findTheEarliestBusPart1(filepath) == result
 
         where:
         filepath       || result
         'day13/01.txt' || 295
         'day13/02.txt' || 102
+    }
+
+    @Unroll
+    def 'should calculate #result the earliest bus multiplied by wait time for file #filepath in part 2'() {
+        expect:
+        new Day13().findTheEarliestBusPart2(filepath) == result
+
+        where:
+        filepath             || result
+        'day13/01.txt'       || 1068781
+        'day13/03.txt'       || 3417
+        'day13/04.txt'       || 754018
+        'day13/05.txt'       || 779210
+        'day13/06.txt'       || 1261476
+        'day13/07.txt'       || 1202161486
+        'day13/02-part2.txt' || 100000000000000
     }
 }
